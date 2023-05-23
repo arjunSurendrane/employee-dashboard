0080f3ad099e2554ad341aee6b08bdb6f962f1d7
Arjun E <arjunrk907@gmail.com>
2023-05-24 03:10:06 +0530

    create routes

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectToLocalhost from "./config/server.js";
 import connectToMongoDB from "./config/database.js";
+import adminRoute from "./routes/admin.js";
+import ownerRoute from "./routes/owner.js";
+import employeeRoute from "./routes/employee.js";
+import authRoute from "./routes/authentication.js";
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
@@ -13,9 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 (function () {
-  app.get("/", (req, res) => {
-    res.send("Welcome");
-  });
+  app.use("/api/v1/auth", authRoute);
+  app.use("/api/v1/admin", adminRoute);
+  app.use("/api/v1/owner", ownerRoute);
+  app.use("/api/v1/employee", employeeRoute);
   connectToMongoDB();
   connectToLocalhost(app, PORT);
 })();
