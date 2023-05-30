0964922de5b3c30798816d8ef5c73edb96b655a0
Arjun E <arjunrk907@gmail.com>
2023-05-31 02:24:05 +0530

    add admin login

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,7 +1,35 @@
 import express from "express";
+import AppError from "../utils/AppError";
 const router = express.Router();
 
-router.get("/", (req, res) => {
+/**
+ * Admin Login
+ */
+router.post("/admin/login", (req, res, next) => {
+  if (!req.body.email && req.body.password)
+    return next(new AppError("Please write email and password", 404));
+  if (
+    !(
+      process.env.ADMIN_MAIL == req.body.email &&
+      process.env.ADMIN_PASS == req.body.password
+    )
+  ) {
+    return next(new AppError("Incorrect email id or password", 404));
+  }
+  res.status(200).json({ status: "success" });
+});
+
+/**
+ * HR Login
+ */
+router.post("/hr/login", (req, res) => {
+  res.send("Welcome to Authentication route");
+});
+
+/**
+ * Employee Login
+ */
+router.post("/employee/login", (req, res) => {
   res.send("Welcome to Authentication route");
 });
 
