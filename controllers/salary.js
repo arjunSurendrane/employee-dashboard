1220da1788e2726fc27e0b7d9a3c18d2eef23ef5
Arjun E <arjunrk907@gmail.com>
2023-06-01 14:28:34 +0530

    add comment in slary controller

--- controllers/salary.js
+++ controllers/salary.js
@@ -1,7 +1,17 @@
 import asynHandler from "express-async-handler";
 import Salary from "../models/salary";
 
-export const salaryData = asynHandler(async (empid) => {
-  const employeeSalary = await Salary.findOne({ empid });
-  return employeeSalary;
-});
+/**
+ * Get salary data
+ */
+export const salaryData = asynHandler(
+  /**
+   * Fetch salary data from database
+   * @param {String} empid
+   * @returns {Object}
+   */
+  async (empid) => {
+    const employeeSalary = await Salary.findOne({ empid });
+    return employeeSalary;
+  }
+);
